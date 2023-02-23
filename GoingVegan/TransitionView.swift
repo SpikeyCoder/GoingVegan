b311d1d708b8d94d8a96b89d54682e75584dea41
Kevin Armstrong <armstr13@vt.edu>
2023-02-23 10:40:30 -0800

    Adding transition loading screen to project

---
+++ GoingVegan/TransitionView.swift
@@ -0,0 +1,37 @@
+//
+//  TransitionView.swift
+//  GoingVegan
+//
+//  Created by Kevin Armstrong on 2/23/23.
+//
+
+import SwiftUI
+
+struct TransitionView: View {
+    @State private var isLoading = false
+    
+    var body: some View {
+            ZStack {
+
+                Circle()
+                    .stroke(Color(.systemGray5), lineWidth: 14)
+                    .frame(width: 100, height: 100)
+
+                Circle()
+                    .trim(from: 0, to: 0.2)
+                    .stroke(Color.green, lineWidth: 7)
+                    .frame(width: 100, height: 100)
+                    .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                    .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false))
+                    .onAppear() {
+                        self.isLoading = true
+                }
+            }
+        }
+}
+
+struct TransitionView_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionView()
+    }
+}
